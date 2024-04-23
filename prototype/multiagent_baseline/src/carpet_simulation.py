7a8e7d1560c8db92bae12174956e44c11b0f99d9
CachoobiDoobi <radu.gaghi@gmail.com>
2024-04-23 12:17:33 +0200

    add multiagent baseline without critic

---
+++ prototype/multiagent_baseline/src/carpet_simulation.py
@@ -0,0 +1,74 @@
+####### our case #######
+import logging
+import math
+import platform
+
+import numpy as np
+from carpet import carpet
+
+from config import param_dict
+
+
+class CarpetSimulation:
+    def __init__(self):
+        if platform.system() == 'Linux':
+            # carpet.read_license("Przlwkofsky")
+            carpet.read_license("/project/carpet3.lcs")
+        # Reset
+        #carpet.reset_radars()
+        # Set conditions
+        #carpet.Clutter_SurfaceClutter = True
+        #carpet.Target_RCS1 = np.random.uniform(2, 20)
+        # carpet.Propagation_WindDirection =
+        #carpet.Propagation_Vwind = np.random.uniform(0, 30)
+
+        # Processing
+        carpet.Processing_DFB = True
+        carpet.Processing_MTI = 'no'
+        carpet.Processing_Integrator = 'm out n'
+        # what is this?
+        carpet.Processing_M = 3
+
+    def detect(self, action_dict, range_, velocity, altitude, wind_speed, rcs, rainfall_rate):
+        carpet.Target_Azimuth = 0
+        carpet.Processing_DFB = True
+        carpet.Processing_MTI = 'no'
+        carpet.Processing_Integrator = 'm out n'
+        carpet.Clutter_SurfaceClutter = True
+        carpet.Target_RCS1 = rcs
+        carpet.Propagation_WindDirection = np.pi
+        carpet.Propagation_Vwind = wind_speed
+        carpet.Clutter_RainPresent = True
+        carpet.Clutter_RainfallRate = rainfall_rate
+        # what is this?
+        carpet.Processing_M = 3
+        for m, agent in enumerate(action_dict):
+            parameters = action_dict[agent]
+            pulse_durations = parameters.get("pulse_duration")
+            n_pulseses = parameters.get('n_pulses')
+            pris = parameters.get('PRI')
+            n_bursts = len(pris)
+            rfs = parameters.get('RF')
+
+            for n in range(1, n_bursts + 1):
+                i = str(n + m * n_bursts)
+                setattr(carpet, f"Transmitter_PRF{i}", 1 / param_dict["PRI"][pris[n - 1]])
+                setattr(carpet, f"Transmitter_Tau{i}", param_dict["pulse_duration"][pulse_durations[n - 1]])
+                setattr(carpet, f"Transmitter_PulsesPerBurst{i}", int(n_pulseses[n - 1]))
+                setattr(carpet,  f"Transmitter_RF{i}", int(param_dict['RF'][rfs[n - 1]]))
+
+
+        assert range_ > 0, "NEGATIVE RANGE"
+
+        carpet.Target_GroundRange = range_
+        carpet.Target_RadialVelocity = velocity
+        carpet.Target_Altitude = altitude
+
+        pds = carpet.detection_probability(ground_ranges=range_, radial_velocities=velocity, altitudes=altitude)
+        scnr = carpet.signal_clutter_noise_power_ratio(ground_ranges=range_, radial_velocities=velocity,altitudes=altitude)
+
+        return (pds, scnr) if not math.isnan(pds) else (0, 0)
+
+    # not thread safe
+    # def getWindSpeed(self):
+    #     return carpet.Propagation_Vwind
---
+++ prototype/multiagent_baseline/src/main.py
@@ -0,0 +1,128 @@
+import os
+import pprint
+
+import ray
+from gymnasium.spaces import Dict, Box, MultiDiscrete
+from ray import tune, air
+from ray.rllib.algorithms import PPOConfig, Algorithm
+from ray.rllib.policy.policy import PolicySpec
+
+from tracking_env import TrackingEnv
+
+agents = [0, 1]
+n_bursts = 3
+
+action_space = Dict(
+    {'pulse_duration': MultiDiscrete(nvec=[5] * n_bursts, start=[0] * n_bursts),
+     'PRI': MultiDiscrete(nvec=[5] * n_bursts, start=[0] * n_bursts),
+     'n_pulses': MultiDiscrete(nvec=[21] * n_bursts, start=[10] * n_bursts),
+     'RF': MultiDiscrete(nvec=[2] * n_bursts),
+     })
+observation_space = Dict(
+    {'pulse_duration': MultiDiscrete(nvec=[5] * n_bursts, start=[0] * n_bursts),
+     'PRI': MultiDiscrete(nvec=[5] * n_bursts, start=[0] * n_bursts),
+     'n_pulses': MultiDiscrete(nvec=[21] * n_bursts, start=[10] * n_bursts),
+     'RF': MultiDiscrete(nvec=[2] * n_bursts),
+     'PD': Box(low=0, high=1),
+     'ratio': Box(low=0, high=100),
+     'r_hat': Box(low=0, high=1e5),
+     'v_hat': Box(low=0, high=1e3),
+     'v_wind': Box(low=0, high=40),
+     'alt': Box(low=10, high=30)
+     }
+)
+
+# TODO testr if we made this much longer
+env_config = {
+    "ts": 20,
+    'agents': agents,
+    # Actions -> [pulse_duration, n_pulses, bandwidth, PRF]
+    'action_space': action_space,
+    # observe actions of other agents, and previous measurement
+    'observation_space': observation_space
+}
+
+config = (
+    PPOConfig()
+    .experimental(_enable_new_api_stack=False)
+    .environment(TrackingEnv, env_config=env_config, clip_actions=True, disable_env_checking=True)
+    .framework('torch')
+    .rollouts(batch_mode="complete_episodes", num_rollout_workers=2)
+    .multi_agent(
+        policies={
+            "pol1": (
+                None,
+                observation_space,
+                action_space,
+                # `framework` would also be ok here.
+                PPOConfig.overrides(framework_str='torch'),
+            ),
+            "pol2": (
+                None,
+                observation_space,
+                action_space,
+                # `framework` would also be ok here.
+                PPOConfig.overrides(framework_str='torch'),
+            ),
+        },
+        policy_mapping_fn=lambda agent_id, episode, worker, **kwargs: "pol1"
+        if agent_id == 0
+        else "pol2",
+    )
+    # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+    .resources(num_gpus=0, num_cpus_per_worker=2)
+    .training(train_batch_size=512, sgd_minibatch_size=128, num_sgd_iter=30)
+)
+
+stop = {
+    "training_iteration": 1,
+    # "time_total_s": 3600 * 14
+    # "episode_reward_mean": 10,
+    # "episodes_total": 900
+}
+
+storage = os.path.abspath("results")
+
+results = tune.Tuner(
+    "PPO",
+    param_space=config.to_dict(),
+    run_config=air.RunConfig(stop=stop, verbose=1,
+                             name="multiagent_baseline", storage_path=storage),).fit()
+
+best_result = results.get_best_result(metric='episode_reward_mean', mode='max', scope='all')
+
+print("\nBest performing trial's final reported metrics:\n")
+
+metrics_to_print = [
+    "episode_reward_mean",
+    "episode_reward_max",
+    "episode_reward_min",
+]
+pprint.pprint({k: v for k, v in best_result.metrics.items() if k in metrics_to_print})
+agent = Algorithm.from_checkpoint(best_result.checkpoint)
+# agent.restore(checkpoint_path=os.path.join(checkpoint_dir, "params.pkl"))
+
+env = TrackingEnv(env_config=config["env_config"])
+
+
+obs, _ = env.reset()
+
+env.wind_speed = 10
+
+env.altitude = 10
+
+env.rcs = 3
+
+env.rainfall_rate = 2.7 * 10e-7
+
+done = False
+while not done:
+    parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1')
+    parameters_2 = agent.compute_single_action(obs[1], policy_id='pol2')
+
+    actions = {0: parameters_1, 1: parameters_2}
+    # print(f"Parameters: {parameters} given observation at previous timestep: {obs}")
+    obs, rewards, terminateds, truncateds, _ = env.step(actions)
+
+    done = terminateds["__all__"]
+env.render()
