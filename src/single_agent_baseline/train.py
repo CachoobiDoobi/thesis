1fcba08f4c6fe8070d63a2356aff50bde0cade9e
CachoobiDoobi <radu.gaghi@gmail.com>
2024-04-23 13:29:55 +0200

    make module

--- src/single_agent_baseline/train.py
+++ src/single_agent_baseline/train.py
@@ -10,7 +10,7 @@ from ray.rllib.algorithms import PPOConfig, Algorithm
 from ray.rllib.policy.policy import PolicySpec
 
 if platform.system() == 'Linux':
-    file_dir = os.path.dirname("/project/src/common")
+    file_dir = os.path.dirname("/project/src/common/")
     sys.path.append(file_dir)
     print(sys.path)
 
