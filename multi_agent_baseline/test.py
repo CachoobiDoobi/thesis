b4265e9f5d5304b234199806616be4a128987f4f
CachoobiDoobi <radu.gaghi@gmail.com>
2024-04-25 16:01:25 +0200

    hmm

--- multi_agent_baseline/test.py
+++ multi_agent_baseline/test.py
@@ -1,32 +1,34 @@
 import ray
+from carpet import carpet
 from gymnasium.spaces import Dict, Box, MultiDiscrete
 from ray.rllib.algorithms import Algorithm
+from ray.rllib.models import ModelCatalog
 
+from model import TorchCentralizedCriticModel
 from tracking_env import TrackingEnv
-
 
 n_bursts = 3
 
 action_space = Dict(
     {'pulse_duration': MultiDiscrete(nvec=[5] * n_bursts, start=[0] * n_bursts),
      'PRI': MultiDiscrete(nvec=[5] * n_bursts, start=[0] * n_bursts),
-     'n_pulses': MultiDiscrete(nvec=[21] * n_bursts, start=[10] * n_bursts)})
+     'n_pulses': MultiDiscrete(nvec=[21] * n_bursts, start=[10] * n_bursts),
+     'RF': MultiDiscrete(nvec=[2] * n_bursts),
+     })
 observation_space = Dict(
     {'pulse_duration': MultiDiscrete(nvec=[5] * n_bursts, start=[0] * n_bursts),
      'PRI': MultiDiscrete(nvec=[5] * n_bursts, start=[0] * n_bursts),
      'n_pulses': MultiDiscrete(nvec=[21] * n_bursts, start=[10] * n_bursts),
+     'RF': MultiDiscrete(nvec=[2] * n_bursts),
      'PD': Box(low=0, high=1),
      'ratio': Box(low=0, high=100),
      'r_hat': Box(low=0, high=1e5),
      'v_hat': Box(low=0, high=1e3),
      'v_wind': Box(low=0, high=40),
      'alt': Box(low=10, high=30)
-     })
-
-# action_space = gymnasium.spaces.utils.flatten_space(action_space)
-# observation_space = gymnasium.spaces.utils.flatten_space(observation_space)
-
-agents = [0]
+     }
+)
+agents = [0, 1]
 
 env_config = {
     "ts": 20,
@@ -39,32 +41,46 @@ env_config = {
 
 ray.init()
 
-cdir = '/nas-tmp/Radu/multi/results/single_agent_baseline/PPO_TrackingEnv_aa42b_00000_0_2024-04-22_15-40-37/checkpoint_000000'
+# ModelCatalog.register_custom_model(
+#     "cc_model",
+#     TorchCentralizedCriticModel
+#
+# )
+
+cdir = '/nas-tmp/Radu/multi/results/multiagent/CentralizedCritic_MultiAgentTrackingEnv_522d5_00000_0_2024-04-19_17-36-14/checkpoint_000000'
 
 agent = Algorithm.from_checkpoint(cdir)
 # agent.restore(checkpoint_path=os.path.join(checkpoint_dir, "params.pkl"))
 
-env =TrackingEnv(env_config=env_config)
+env = TrackingEnv(env_config=env_config)
+pds = []
+ratios = []
+track = []
 
-obs, _ = env.reset()
+num_iterations = 1000
+for _ in range(num_iterations):
 
-env.wind_speed = 40
+    obs, _ = env.reset()
 
-env.altitude = 10
+    env.wind_speed = 40
 
-env.rcs = 3
+    env.altitude = 10
 
-env.rainfall_rate = 2.7 * 10e-7
+    env.rcs = 3
 
-done = False
-while not done:
-    parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1')
-    parameters_2 = agent.compute_single_action(obs[1], policy_id='pol2')
+    env.rainfall_rate = 2.7 * 10e-7
+    done = False
+    while not done:
+        parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1')
+        parameters_2 = agent.compute_single_action(obs[1], policy_id='pol2')
 
-    actions = {0: parameters_1, 1: parameters_2}
-    print(f"Parameters: {None} given observation at previous timestep: {obs}")
-    obs, rewards, terminateds, truncateds, _ = env.step(actions)
+        actions = {0: parameters_1, 1: parameters_2}
+        # print(f"Parameters: {None} given observation at previous timestep: {obs}")
+        obs, rewards, terminateds, truncateds, _ = env.step(actions)
 
-    done = terminateds["__all__"]
+        done = terminateds["__all__"]
+    pds.append(env.pds)
+    ratios.append(env.ratios)
+    track.append(carpet.firm_track_probability(env.pds))
 
-env.render()
+env.render_with_variance(pds=pds, ratios=ratios, track_probs=track)
