b56cfb066162b39a02ccd5fdaa4a63b5bbf6f0ac
CachoobiDoobi <radu.gaghi@gmail.com>
2024-05-21 16:27:09 +0200

    best thing heatmap

--- hist_grid.py
+++ hist_grid.py
@@ -5,35 +5,43 @@ from plotly.subplots import make_subplots
 
 path = ""
 
-fig = make_subplots(2, 3)
+layout = go.Layout(
+        title='Waveform distribution',
+        xaxis=dict(title='Firm track probability'),
+        yaxis=dict(title='Waveform duration ratio')
+    )
+
+fig = make_subplots(2, 3, subplot_titles=["Single Agent Baseline", "Multi Agent Baseline", "Multi Agent Critic (FC Net)", "Multi Agent Critic (GNN Stochastic)", "Multi Agent Critic (GNN FC)", "Multi Agent Critic (GNN Threshold)"], x_title='Firm track probability', y_title='Waveform duration ratio',)
 fig.add_trace(go.Histogram2d(
     x = np.loadtxt("single_agent_baseline/results/track.txt"),
-    y = np.loadtxt(path+"single_agent_baseline/results/ratios.txt"),
+    y = np.loadtxt(path+"single_agent_baseline/results/ratios.txt"),zmin=0, zmax=0.3,bingroup=1,histnorm='probability',
     coloraxis = "coloraxis",), 1, 1)
 
 fig.add_trace(go.Histogram2d(
-    x = np.loadtxt(path+"single_agent_baseline/results/track.txt"),
-    y = np.loadtxt(path+"single_agent_baseline/results/ratios.txt"),
+    x = np.loadtxt(path+"multi_agent_baseline/results/track.txt"),
+    y = np.loadtxt(path+"multi_agent_baseline/results/ratios.txt"),zmin=0, zmax=0.3,bingroup=1,histnorm='probability',
     coloraxis = "coloraxis",), 1, 2)
 
 fig.add_trace(go.Histogram2d(
-    x = np.loadtxt(path+"single_agent_baseline/results/track.txt"),
-    y = np.loadtxt(path+"single_agent_baseline/results/ratios.txt"),
+    x = np.loadtxt(path+"multi_agent_critic_fc/results/track.txt"),
+    y = np.loadtxt(path+"multi_agent_critic_fc/results/ratios.txt"),zmin=0, zmax=0.3,bingroup=1,histnorm='probability',
     coloraxis = "coloraxis",), 1, 3)
 
 fig.add_trace(go.Histogram2d(
-    x = np.loadtxt(path+"single_agent_baseline/results/track.txt"),
-    y = np.loadtxt(path+"single_agent_baseline/results/ratios.txt"),
+    x = np.loadtxt(path+"multi_agent_critic_gnn/results/track.txt"),
+    y = np.loadtxt(path+"multi_agent_critic_gnn/results/ratios.txt"),zmin=0, zmax=0.3,bingroup=1,histnorm='probability',
     coloraxis = "coloraxis",), 2, 1)
 
 fig.add_trace(go.Histogram2d(
-    x = np.loadtxt(path+"single_agent_baseline/results/track.txt"),
-    y = np.loadtxt(path+"single_agent_baseline/results/ratios.txt"),
+    x = np.loadtxt(path+"multi_agent_critic_gnn_fc_graph/results/track.txt"),
+    y = np.loadtxt(path+"multi_agent_critic_gnn_fc_graph/results/ratios.txt"),zmin=0, zmax=0.3,bingroup=1,histnorm='probability',
     coloraxis = "coloraxis",), 2, 2)
 
 fig.add_trace(go.Histogram2d(
-    x = np.loadtxt(path+"single_agent_baseline/results/track.txt"),
-    y = np.loadtxt(path+"single_agent_baseline/results/ratios.txt"),
+    x = np.loadtxt(path+"multi_agent_critic_gnn_threshold/results/track.txt"),
+    y = np.loadtxt(path+"multi_agent_critic_gnn_threshold/results/ratios.txt"),zmin=0, zmax=0.3,bingroup=1,histnorm='probability',
     coloraxis = "coloraxis",), 2, 3)
+
+
 
 fig.show()
--- single_agent_baseline/test.py
+++ single_agent_baseline/test.py
@@ -6,7 +6,7 @@ from ray.rllib import RolloutWorker
 from ray.rllib.algorithms import Algorithm, PPOConfig
 from ray.rllib.policy.policy import PolicySpec
 
-from utils import plot_2d_hist
+from utils import plot_2d_hist, plot_heatmaps_rcs_wind, plot_heatmaps_rcs_rainfall, plot_heatmaps_wind_rainfall
 from tracking_env import TrackingEnv
 
 agents = [0]
@@ -53,166 +53,174 @@ pds = []
 ratios = []
 track = []
 
-num_iterations = 100
-for i in range(num_iterations):
-    print(i)
-    obs, _ = env.reset()
-
-    env.wind_speed = 40
-
-    env.altitude = 10
-
-    env.rcs = 1
-
-    env.rainfall_rate = 2.7 * 10e-7
-    done = False
-    while not done:
-        parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1', explore=False)
-
-        actions = {0: parameters_1}
-        # print(f"Parameters: {None} given observation at previous timestep: {obs}")
-        obs, rewards, terminateds, truncateds, _ = env.step(actions)
-
-        done = terminateds["__all__"]
-    pds.append(env.pds)
-    ratios.append(env.ratios)
-    track.append(carpet.firm_track_probability(env.pds))
-
-pds = np.array(pds).reshape(-1)
-pds = np.round(pds, decimals=2)
-
-ratios = np.array(ratios).reshape(-1)
-ratios = np.round(ratios, decimals=2)
-
-track = np.array(track).reshape(-1)
-track = np.round(track, decimals=2)
-
-
-np.savetxt("/project/single_agent_baseline/results/pds.txt", pds)
-np.savetxt("/project/single_agent_baseline/results/ratios.txt", ratios)
-np.savetxt("/project/single_agent_baseline/results/track.txt", track)
-plot_2d_hist(track, ratios)
+# num_iterations = 100
+# for i in range(num_iterations):
+#     print(i)
+#     obs, _ = env.reset()
+#
+#     env.wind_speed = 40
+#
+#     env.altitude = 10
+#
+#     env.rcs = 1
+#
+#     env.rainfall_rate = 2.7 * 10e-7
+#     done = False
+#     while not done:
+#         parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1', explore=False)
+#
+#         actions = {0: parameters_1}
+#         # print(f"Parameters: {None} given observation at previous timestep: {obs}")
+#         obs, rewards, terminateds, truncateds, _ = env.step(actions)
+#
+#         done = terminateds["__all__"]
+#     pds.append(env.pds)
+#     ratios.append(env.ratios)
+#     track.append(carpet.firm_track_probability(env.pds))
+#
+# pds = np.array(pds).reshape(-1)
+# pds = np.round(pds, decimals=2)
+#
+# ratios = np.array(ratios).reshape(-1)
+# ratios = np.round(ratios, decimals=2)
+#
+# track = np.array(track).reshape(-1)
+# track = np.round(track, decimals=2)
+#
+#
+# np.savetxt("/project/single_agent_baseline/results/pds.txt", pds)
+# np.savetxt("/project/single_agent_baseline/results/ratios.txt", ratios)
+# np.savetxt("/project/single_agent_baseline/results/track.txt", track)
+# plot_2d_hist(track, ratios)
 
 # env.render_with_variance(pds=pds, ratios=ratios, track_probs=track)
 # env.render_hist(pds=pds, ratios=ratios, track_probs=track)
 # env.render_hist_treshold(pds=pds, ratios=ratios, track_probs=track)
 
 
-# pds = np.zeros((20, 20))
-# ratios = np.zeros((20, 20))
-# track = np.zeros((20, 20))
-#
-# rcs = np.linspace(1, 20, num=20)
-# wind_speed = np.linspace(start=0, stop=40, num=20)
-# rainfall_rate = np.linspace(start=0, stop=2.8 * 1e-6, num=20)
-#
-# num_iterations = 100
-#
-# for i, r in enumerate(rcs):
-#     for j, w in enumerate(wind_speed):
-#         for _ in range(num_iterations):
-#
-#             obs, _ = env.reset()
-#
-#             env.wind_speed = w
-#
-#             env.rcs = r
-#
-#             env.rainfall_rate = 2.7 * 10e-7
-#
-#             env.altitude = 10
-#
-#             done = False
-#             while not done:
-#                 parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1')
-#
-#                 actions = {0: parameters_1}
-#                 # print(f"Parameters: {None} given observation at previous timestep: {obs}")
-#                 obs, rewards, terminateds, truncateds, _ = env.step(actions)
-#
-#                 done = terminateds["__all__"]
-#             pds[i, j] += np.mean(env.pds)
-#             ratios[i, j] += np.mean(env.ratios)
-#             track[i, j] += np.mean(carpet.firm_track_probability(env.pds))
-#
-# pds = pds / num_iterations
-# ratios = ratios / num_iterations
-# track = track / num_iterations
-#
-# plot_heatmaps_rcs_wind(pds, ratios, track)
-#
-# pds = np.zeros((20, 20))
-# ratios = np.zeros((20, 20))
-# track = np.zeros((20, 20))
-#
-# for i, r in enumerate(rcs):
-#     for j, w in enumerate(rainfall_rate):
-#         for _ in range(num_iterations):
-#
-#             obs, _ = env.reset()
-#
-#             env.wind_speed = 40
-#
-#             env.rcs = r
-#
-#             env.rainfall_rate = w
-#
-#             env.altitude = 10
-#
-#             done = False
-#             while not done:
-#                 parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1')
-#
-#                 actions = {0: parameters_1}
-#                 # print(f"Parameters: {None} given observation at previous timestep: {obs}")
-#                 obs, rewards, terminateds, truncateds, _ = env.step(actions)
-#
-#                 done = terminateds["__all__"]
-#             pds[i, j] += np.mean(env.pds)
-#             ratios[i, j] += np.mean(env.ratios)
-#             track[i, j] += np.mean(carpet.firm_track_probability(env.pds))
-#
-# pds = pds / num_iterations
-# ratios = ratios / num_iterations
-# track = track / num_iterations
-#
-# plot_heatmaps_rcs_rainfall(pds, ratios, track)
-#
-# ########################
-#
-# pds = np.zeros((20, 20))
-# ratios = np.zeros((20, 20))
-# track = np.zeros((20, 20))
-#
-# for i, w in enumerate(wind_speed):
-#     for j, r in enumerate(rainfall_rate):
-#         for _ in range(num_iterations):
-#
-#             obs, _ = env.reset()
-#
-#             env.wind_speed = w
-#
-#             env.rcs = 1
-#
-#             env.rainfall_rate = r
-#
-#             env.altitude = 10
-#
-#             done = False
-#             while not done:
-#                 parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1')
-#
-#                 actions = {0: parameters_1}
-#                 # print(f"Parameters: {None} given observation at previous timestep: {obs}")
-#                 obs, rewards, terminateds, truncateds, _ = env.step(actions)
-#
-#                 done = terminateds["__all__"]
-#             pds[i, j] += np.mean(env.pds)
-#             ratios[i, j] += np.mean(env.ratios)
-#             track[i, j] += np.mean(carpet.firm_track_probability(env.pds))
-#
-# pds = pds / num_iterations
-# ratios = ratios / num_iterations
-# track = track / num_iterations
-#
-# plot_heatmaps_wind_rainfall(pds, ratios, track)
+pds = np.zeros((20, 20))
+ratios = np.zeros((20, 20))
+track = np.zeros((20, 20))
+
+rcs = np.linspace(1, 20, num=20)
+wind_speed = np.linspace(start=0, stop=40, num=20)
+rainfall_rate = np.linspace(start=0, stop=2.8 * 1e-6, num=20)
+
+num_iterations = 1
+
+for i, r in enumerate(rcs):
+    for j, w in enumerate(wind_speed):
+        for _ in range(num_iterations):
+
+            obs, _ = env.reset()
+
+            env.wind_speed = w
+
+            env.rcs = r
+
+            env.rainfall_rate = 2.7 * 10e-7
+
+            env.altitude = 10
+
+            done = False
+            max_reward = 0
+            index = -1
+            i = 0
+            while not done:
+                parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1')
+
+                actions = {0: parameters_1}
+                # print(f"Parameters: {None} given observation at previous timestep: {obs}")
+                obs, rewards, terminateds, truncateds, _ = env.step(actions)
+                if np.sum(rewards.values()) > max_reward:
+                    max_reward = np.sum(rewards.values())
+                    index = i
+                done = terminateds["__all__"]
+                i += 1
+            pds[i, j] = env.pds[i]
+            ratios[i, j] = env.ratios[i]
+            track[i, j] = carpet.firm_track_probability(env.pds)[i]
+
+
+plot_heatmaps_rcs_wind(pds, ratios, track)
+
+pds = np.zeros((20, 20))
+ratios = np.zeros((20, 20))
+track = np.zeros((20, 20))
+
+for i, r in enumerate(rcs):
+    for j, w in enumerate(rainfall_rate):
+        for _ in range(num_iterations):
+
+            obs, _ = env.reset()
+
+            env.wind_speed = 40
+
+            env.rcs = r
+
+            env.rainfall_rate = w
+
+            env.altitude = 10
+
+            done = False
+            max_reward = 0
+            index = -1
+            i = 0
+            while not done:
+                parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1')
+
+                actions = {0: parameters_1}
+                # print(f"Parameters: {None} given observation at previous timestep: {obs}")
+                obs, rewards, terminateds, truncateds, _ = env.step(actions)
+                if np.sum(rewards.values()) > max_reward:
+                    max_reward = np.sum(rewards.values())
+                    index = i
+                done = terminateds["__all__"]
+                i += 1
+            pds[i, j] = env.pds[i]
+            ratios[i, j] = env.ratios[i]
+            track[i, j] = carpet.firm_track_probability(env.pds)[i]
+
+
+plot_heatmaps_rcs_rainfall(pds, ratios, track)
+
+########################
+
+pds = np.zeros((20, 20))
+ratios = np.zeros((20, 20))
+track = np.zeros((20, 20))
+
+for i, w in enumerate(wind_speed):
+    for j, r in enumerate(rainfall_rate):
+        for _ in range(num_iterations):
+
+            obs, _ = env.reset()
+
+            env.wind_speed = w
+
+            env.rcs = 1
+
+            env.rainfall_rate = r
+
+            env.altitude = 10
+
+            done = False
+            max_reward = 0
+            index = -1
+            i = 0
+            while not done:
+                parameters_1 = agent.compute_single_action(obs[0], policy_id='pol1')
+
+                actions = {0: parameters_1}
+                # print(f"Parameters: {None} given observation at previous timestep: {obs}")
+                obs, rewards, terminateds, truncateds, _ = env.step(actions)
+                if np.sum(rewards.values()) > max_reward:
+                    max_reward = np.sum(rewards.values())
+                    index = i
+                done = terminateds["__all__"]
+                i += 1
+            pds[i, j] = env.pds[i]
+            ratios[i, j] = env.ratios[i]
+            track[i, j] = carpet.firm_track_probability(env.pds)[i]
+
+plot_heatmaps_wind_rainfall(pds, ratios, track)
