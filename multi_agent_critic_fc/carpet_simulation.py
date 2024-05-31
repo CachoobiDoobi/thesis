b91d51390d3e086d9d33b4a5eb1157997196927b
CachoobiDoobi <radu.gaghi@gmail.com>
2024-05-31 10:32:33 +0200

    add ebst haetmap code

--- multi_agent_critic_fc/carpet_simulation.py
+++ multi_agent_critic_fc/carpet_simulation.py
@@ -1,4 +1,4 @@
-
+import logging
 import math
 import platform
 
@@ -12,7 +12,7 @@ class CarpetSimulation:
     def __init__(self):
         if platform.system() == 'Linux':
             # carpet.read_license("Przlwkofsky")
-            carpet.read_license("/project/misc/carpet3.lcs")
+            carpet.read_license("/project/carpet3.lcs")
         # Processing
         carpet.Processing_DFB = True
         carpet.Processing_MTI = 'no'
@@ -31,6 +31,8 @@ class CarpetSimulation:
         carpet.Propagation_Vwind = wind_speed
         carpet.Clutter_RainPresent = True
         carpet.Clutter_RainfallRate = rainfall_rate
+        carpet.Clutter_RainRange = 0
+        carpet.Clutter_RainDiameter = 1e6
         # what is this?
         carpet.Processing_M = 3
         for m, agent in enumerate(action_dict):
@@ -47,6 +49,7 @@ class CarpetSimulation:
                 setattr(carpet, f"Transmitter_Tau{i}", param_dict["pulse_duration"][pulse_durations[n - 1]])
                 setattr(carpet, f"Transmitter_PulsesPerBurst{i}", int(n_pulseses[n - 1]))
                 setattr(carpet,  f"Transmitter_RF{i}", int(param_dict['RF'][rfs[n - 1]]))
+                setattr(carpet, f"Transmitter_NrFilters{i}", int(n_pulseses[n - 1]))
 
 
         assert range_ > 0, "NEGATIVE RANGE"
